c92c71fe4206be3a307aa1bb4244b94fab96de66
Keval Dhanani <dhanani.kev01@gmail.com>
2021-10-17 22:49:39 +0530

    Login Functionality Completed

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 from flask import Flask, render_template, abort, request, redirect, session
 from flask_pymongo import PyMongo
 from cfg import config
+from utils import get_random_string
 import json
 from hashlib import sha256
 from datetime import datetime
@@ -30,8 +31,15 @@ def login():
     if 'registerSuccess' in session:
         alertMesage = session['registerSuccess']
         session.pop('registerSuccess', None)
+
+    error = ''
+    # Add error message if any
+    if 'error' in session:
+        error = session['error']
+        session.pop('error', None)
+
     session.pop('value', None)
-    return render_template('login.html', title='ShareBytes | Login', alertMesage=alertMesage)
+    return render_template('login.html', title='ShareBytes | Login', alertMesage=alertMesage, error=error)
 
 # Displays register page
 @app.route('/get-started')
@@ -47,6 +55,22 @@ def register():
         default_form_values = session['value']
         session.pop('value', None)
     return render_template('register.html', title='ShareBytes | Getting Started', error=error, value=default_form_values)
+
+# Displays the main page after successful login
+@app.route('/main')
+def main():
+    if not 'userToken' in session:
+        session['error'] = 'You must login to access this page'
+        return redirect('/login')
+
+    token_document = mongo.db.user_tokens.find_one({
+        'sessionHash': session['userToken']
+    })
+    if token_document is None:
+        session.pop('userToken', None)
+        session['error'] = 'You must login again to access this page'
+        return redirect('/login')
+    return 'Main Page'
 
 @app.route('/handle-register', methods=['POST'])
 def handleRegister():
@@ -102,6 +126,42 @@ def handleRegister():
         return redirect('/login')
     session['error'] = 'Invalid Request'
     return redirect('/get-started')
+
+@app.route('/check-login', methods=['POST'])
+def checkLogin():
+    if request.method == 'POST':
+        email = request.form['login-email'].strip()
+        password = request.form['login-password'].strip()
+        # Check if email is empty
+        if not len(email) > 0:
+            session['error'] = 'Email is required'
+            return redirect('/login')
+        # Check if password is empty
+        if not len(password) > 0:
+            session['error'] = 'Password is required'
+            return redirect('/login')
+        # Check if email present in database
+        user_document = mongo.db.users.find_one({ 'email': email })
+        if user_document is None:
+            session['error'] = 'No account exists with this email address'
+            return redirect('/login')
+        # Check if password hash matches
+        password_hash = sha256(password.encode('utf-8')).hexdigest()
+        if user_document['password'] != password_hash:
+            session['error'] = 'Password does not match'
+            return redirect('/login')
+
+        # Generate token and save it in session
+        random_string = get_random_string()
+        randomSessionHash = sha256(random_string.encode('utf-8')).hexdigest()
+        token_object = mongo.db.user_tokens.insert_one({
+            'userID': user_document['_id'],
+            'sessionHash': randomSessionHash,
+            'createdAt': datetime.utcnow()
+        })
+
+        session['userToken'] = randomSessionHash
+        return redirect('/main')
 
 if __name__ == '__main__':
     app.run(debug=True) # Debug set to True for development purpose
