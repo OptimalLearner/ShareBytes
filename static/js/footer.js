84fd8bfbc85bb37bd5ad574c2d0215c058dfc9b6
Keval Dhanani <dhanani.kev01@gmail.com>
2021-11-18 20:28:10 +0530

    Template Block Added for Footer + Template Block Included in Required Files + JS Script for Footer Added in Separate File

---
+++ static/js/footer.js
@@ -0,0 +1,37 @@
+document.querySelector(".newsletter-btn").addEventListener("click", () => {
+    let emailForNewsletter = document.querySelector(".newsletter-email").value;
+    if(emailForNewsletter== '') {
+        alert('No value entered');
+        return;
+    }
+    // Email Validation
+    if(!validateEmail(emailForNewsletter)) {
+        return;
+    }
+    fetch('http://localhost:5000/subscribe-to-newsletter', {
+        method: "POST",
+        headers : { 
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify({newsletterEmail: emailForNewsletter})
+    })
+    .then(
+        res => res.json()
+    ).then(
+        res => alert(res['message'])
+    ).catch(
+        err => console.log("Error: " + err)
+    );
+    document.querySelector(".newsletter-email").value = '';
+});
+
+function validateEmail(email) {
+    var validRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if (email.match(validRegex)) {
+        return true;
+    } else {
+        alert("Invalid email address!");      
+        return false;
+    }
+}
