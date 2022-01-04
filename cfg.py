7be9c6b486acecc6810688873c30442da798601c
Keval Dhanani <dhanani.kev01@gmail.com>
2022-01-04 23:56:28 +0530

    MongoDB Atlas URL Changed To Non-SRV Format

--- cfg.py
+++ cfg.py
@@ -1,5 +1,5 @@
 import os
 
 config = {
-    'mongo_uri': f'mongodb+srv://{os.environ.get("MONGO_USER")}:{os.environ.get("MONGO_PASSWORD")}@{os.environ.get("MONGO_CLUSTER")}.pcszv.mongodb.net/{os.environ.get("MONGO_DB")}?retryWrites=true&w=majority'
+    'mongo_uri': f'mongodb://{os.environ.get("MONGO_USER")}:{os.environ.get("MONGO_PASSWORD")}@cluster0-shard-00-00.pcszv.mongodb.net:27017,cluster0-shard-00-01.pcszv.mongodb.net:27017,cluster0-shard-00-02.pcszv.mongodb.net:27017/{os.environ.get("MONGO_DB")}?ssl=true&replicaSet=atlas-duk28h-shard-0&authSource=admin&retryWrites=true&w=majority'
 }
